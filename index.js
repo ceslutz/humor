1d86b096848a13769a2926db492c2c31ab89b1d1
Ces Lutz <ceslutz@icloud.com>
2022-08-27 21:13:17 -0300

    una broma

---
+++ index.js
@@ -0,0 +1,47 @@
+
+// if(2){
+//     console.log("entramos");
+// }
+// console.log("terminamos");
+
+// let respuesta = prompt("terminaste la tarea");
+// if(respuesta.toLowerCase() === "si"){
+//     alert("Puedes salir a jugar");
+// }else{
+//     alert("Te quedas en casa");
+// 
+// let edad = parseInt(prompt("ingresa tu edad"));
+// if(edad < 15){
+//     alert ("no puedes entrar");
+// }else if(edad <= 17){
+//     alert ("puedes ingresar con un mayor responsable");
+// }else{
+//     alert ("bienvenido a la fiesta")
+//}
+//    let respuestaTarea = prompt("terminaste la tarea");
+//    let respuestaCasa = prompt("ordenaste tu pieza");
+//    if(respuestaTarea.toLowerCase() === "si" && respuestaCasa.toLowerCase() === "si"){
+//     alert("puedes ir a fútbol");
+//    }else{
+//     alert("te quedas en casa");
+//    }
+//  let respuestaTarea = prompt("terminaste la tarea");
+//    let respuestaCasa = prompt("ordenaste tu pieza");
+//    if(respuestaTarea.toLowerCase() === "si" || respuestaCasa.toLowerCase() === "si"){
+//     alert("puedes ir a fútbol");
+//    }else{
+//     alert("te quedas en casa");
+//    }
+// let nombre = prompt("Ingresa el nombre");
+// if((nombre !== "") && (nombre === "cesar" || nombre === "CESAR")){
+//     alert("Hola Cesar");
+// }else{
+//     alert("valor incorrecto");
+// }
+
+  let hincha = prompt("Sos Tatengue");
+  if(hincha.toLowerCase() === "si"){
+    alert("nunca saliste campeón")
+  }else{
+    alert("no me mientas, la tenés adentro");
+  }
